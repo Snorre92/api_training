2085e7f66aaafd308be46963acc37d3e03d1aed6
Snorre92 <charfnew@gmail.com>
2021-03-13 21:04:55 +0100

    début de l'api retrofit

--- src/main/java/fr/esiea/ex4A/hello/AgigyService.java
+++ src/main/java/fr/esiea/ex4A/hello/AgigyService.java
@@ -1,4 +1,11 @@
 package fr.esiea.ex4A.hello;
 
-public interface APIInterface {
+import org.springframework.http.ResponseEntity;
+import retrofit2.Response;
+import retrofit2.http.POST;
+
+
+public interface AgigyService {
+    @POST("/api/inscription")
+    Response<ResponseEntity>
 }
--- src/main/java/fr/esiea/ex4A/hello/HelloController.java
+++ src/main/java/fr/esiea/ex4A/hello/HelloController.java
@@ -1,9 +1,12 @@
 package fr.esiea.ex4A.hello;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 @RestController
 class HelloController {
@@ -23,5 +26,40 @@ class HelloController {
             helloData = helloRepository.getHelloFor(name);
         }
         return helloData;
+    }
+
+    @PostMapping(path = "/api/inscription", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object>responseEntity(@RequestBody Map<String, String> body) {
+        if(body.containsKey("userEmail")
+            && body.containsKey("userName")
+            && body.containsKey("userTweeter")
+            && body.containsKey("userCountry")
+            && body.containsKey("userSex")
+            && body.containsKey("userSexPref")) {
+            if(body.get("userSex").matches("^[OMF]$") && body.get("userSexPref").matches("^[OMF]$")) {
+                System.out.println(body);
+                return ResponseEntity.status(201).body(body);
+            }
+        }
+        //String userEmail = (String) json.get("userEmail");
+        //String userName = (String)  json.get("userName");
+        //String userTweeter = (String) json.get("userTweeter");
+        //String userCountry = (String) json.get("userCountry");
+        //String userSex = (String) json.get("userSex");
+        //String userSexPref = (String) json.get("userSexPref");
+        //return ResponseEntity
+        return ResponseEntity.status(404).body("My bad master Darth Vader");
+    }
+
+    @GetMapping(path = "/api/matches", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<Object>matche(@RequestParam(name = "userName") String userName, @RequestParam(name = "userCountry") String userCountry) {
+        final List<Map<String, String>> profiles = List.of(
+            Map.of("name", "machin","twitter","machin45"),
+            Map.of("name","Charf-Eddin Belhadj", "twitter", "Zboubosor Fin Stratège du Cul"),
+            Map.of("name","Loick Caillon","twitter","Fin puceau des abysses"),
+            Map.of("name","Alexandre Boutrig", "twitter", "Branlix3000"),
+            Map.of("name","truc","twitter","bidule-chouette")
+        );
+        return ResponseEntity.ok().body(profiles.get(new Random().nextInt(profiles.size())));
     }
 }
